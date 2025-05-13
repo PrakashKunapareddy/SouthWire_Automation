5ee038cdf262ccec2edcb09f98fcadba1b3092ce
PrakashKunapareddy <saiprakash.kunapareddy@vassarlabs.com>
2025-05-13 14:21:45 +0530

    second

--- test-output/report.js
+++ test-output/report.js
@@ -1,21 +1,141 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "f9904408-d151-48ca-b895-f2a5e7b5bb03",
-    "feature": "Sample feature file",
-    "scenario": "Sample scenario",
-    "start": 1691045071161,
-    "group": 14,
+    "id": "20b6fc69-29ad-42a7-b061-8253cfa847db",
+    "feature": "Login Functionality for SouthWire Login",
+    "scenario": "Successful Login with valid username and password",
+    "start": 1747119522948,
+    "group": 20,
     "content": "",
     "tags": "",
-    "end": 1691045074411,
-    "className": "passed"
+    "end": 1747119654290,
+    "className": "failed"
+  },
+  {
+    "id": "8d47c486-f235-4c87-854d-677db13c81c4",
+    "feature": "Login Functionality for SouthWire Login",
+    "scenario": "Successful Login with valid username and password",
+    "start": 1747119522953,
+    "group": 17,
+    "content": "",
+    "tags": "",
+    "end": 1747119652649,
+    "className": "failed"
+  },
+  {
+    "id": "6d3fa1d6-0241-4ca8-9b74-37705754d357",
+    "feature": "Login Functionality for SouthWire Login",
+    "scenario": "Successful Login with valid username and password",
+    "start": 1747119522951,
+    "group": 19,
+    "content": "",
+    "tags": "",
+    "end": 1747119626934,
+    "className": "failed"
+  },
+  {
+    "id": "6ba21b7c-046e-4426-80f1-2d28734a0f0a",
+    "feature": "Utility Dashboard Data Validation For First Four Dashboards",
+    "scenario": "Validate Energy Sources dashboard data for selected company group against Excel Data",
+    "start": 1747119522924,
+    "group": 16,
+    "content": "",
+    "tags": "",
+    "end": 1747119649459,
+    "className": "failed"
+  },
+  {
+    "id": "67ce6b51-82cf-45f9-b7b4-52440a2c6016",
+    "feature": "Utility Dashboard Data Validation",
+    "scenario": "Validate utility dashboard data for selected company against Excel Data",
+    "start": 1747119522933,
+    "group": 15,
+    "content": "",
+    "tags": "",
+    "end": 1747119745028,
+    "className": "failed"
+  },
+  {
+    "id": "d8e47e09-002c-494a-a2bb-717b6a436aea",
+    "feature": "Login Functionality for SouthWire Login",
+    "scenario": "Successful Login with valid username and password",
+    "start": 1747119522939,
+    "group": 21,
+    "content": "",
+    "tags": "",
+    "end": 1747119652159,
+    "className": "failed"
+  },
+  {
+    "id": "25e34424-e473-4472-901a-05bfb8e20a10",
+    "feature": "Login Functionality for SouthWire Login",
+    "scenario": "Successful Login with valid username and password",
+    "start": 1747119522816,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1747119667633,
+    "className": "failed"
+  },
+  {
+    "id": "a3cb3b43-59bb-4013-8e7a-c487adcbfa54",
+    "feature": "Login Functionality for SouthWire Login",
+    "scenario": "Successful Login with valid username and password",
+    "start": 1747119522949,
+    "group": 22,
+    "content": "",
+    "tags": "",
+    "end": 1747119652359,
+    "className": "failed"
+  },
+  {
+    "id": "4c826ed9-4975-4dc7-baad-81f0fa3914e4",
+    "feature": "Dashboard UT, GEN, TD-SYS Functionality",
+    "scenario": "Data in the dashboard UT, GEN, TD-SYS with multiple filters should match the given data",
+    "start": 1747119522822,
+    "group": 23,
+    "content": "",
+    "tags": "",
+    "end": 1747119631439,
+    "className": "failed"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
-    "id": 14,
+    "id": 15,
     "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 16,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 17,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[TestNG-PoolService-3,5,main]"
+  },
+  {
+    "id": 19,
+    "content": "Thread[TestNG-PoolService-4,5,main]"
+  },
+  {
+    "id": 20,
+    "content": "Thread[TestNG-PoolService-5,5,main]"
+  },
+  {
+    "id": 21,
+    "content": "Thread[TestNG-PoolService-6,5,main]"
+  },
+  {
+    "id": 22,
+    "content": "Thread[TestNG-PoolService-7,5,main]"
+  },
+  {
+    "id": 23,
+    "content": "Thread[TestNG-PoolService-8,5,main]"
   }
 ]);
 });
